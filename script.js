23209c6cccf2182b6cbda5936f65d62622e8d7ff
Mehmet Şirin Yılmaz <msrnylmz@gmail.com>
2025-05-15 11:25:51 +0300

    #items.json

--- script.js
+++ script.js
@@ -12,7 +12,26 @@ function ToogleHeader() {
 	header.classList.toggle('active');
 }
 
+fetch('items.json')
+    .then(response => response.json()) 
+    .then(portfolioItems => {
+        const container = document.getElementById('portfolioContainer');
+        
+        portfolioItems.forEach(item => {
+            const box = document.createElement('div');
+            box.classList.add('box');
+
+            box.innerHTML = `
+                <img src="${item.image}" alt="${item.title}">
+                <p class="appNameL">${item.title}</p>
+                <div class="icons">
+					<a href="${item.url}" target="_blank">
+                    	<img src="${item.logo}"  alt="Logo">
+					</a>
+                </div>
+            `;
+
+            container.appendChild(box);
+        });
+    });
 };
-
-
-
